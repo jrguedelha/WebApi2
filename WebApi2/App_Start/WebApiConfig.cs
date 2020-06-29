a9518bb3567ed6bd67a153829b225d9fce6053d7
jrguedelha <jrguedelha@gmail.com>
2020-06-29 13:00:35 -0300

    WebApiConfig.cs editado para forcar a resposta em JSON

--- WebApi2/App_Start/WebApiConfig.cs
+++ WebApi2/App_Start/WebApiConfig.cs
@@ -19,6 +19,9 @@ namespace WebApi2
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
         }
     }
 }
